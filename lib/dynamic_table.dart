3ff56c7d03a0c807f2af6e5ea41b4948c20eecfb
selva-bizzgit <154110837+selva-bizzgit@users.noreply.github.com>
2023-12-31 20:13:49 +0000

    Update dynamic_table.dart minor changes

--- lib/dynamic_table.dart
+++ lib/dynamic_table.dart
@@ -6,3 +6,5 @@ export 'dynamic_table_data_cell.dart';
 export 'dynamic_table_data_column.dart';
 export 'dynamic_table_data_row.dart';
 export 'dynamic_input_type/dynamic_table_input_type.dart';
+
+//TODO: Try unfocusing and disposing previous focus nodes and other resources in onwidgetupdate method
